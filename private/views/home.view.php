e9106e59e3009d2235d28b9bd7b9be71b398c80c
Gerald <jhongeraldvillalon@gmail.com>
2023-11-07 13:45:04 +0800

    Add a includes directory in view directory

--- private/views/home.view.php
+++ private/views/home.view.php
@@ -1,11 +1,19 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Home</title>
+
+    <link rel="stylesheet" href="assets/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/all.min.css">
 </head>
+
 <body>
-    <h1>Home View</h1>
+    <div class="container-fluid">
+        <h1><i class="fa fa-plus"></i>Home View</h1>
+    </div>
 </body>
+
 </html>
