00c04e054bc0524f212d33375e5775b0d54c81a2
Gerald <jhongeraldvillalon@gmail.com>
2023-11-08 18:23:54 +0800

    Add CRUD

--- private/controllers/Home.php
+++ private/controllers/Home.php
@@ -5,12 +5,17 @@ class Home extends Controller
     public function index()
     {
         $user = new User();
+
+        // $user->insert($arr);
+        // $user->update(3, $arr);
+        $user->delete(3);
+
         $data = $user->findAll();
         // $data = $user->where("first_name", 'Jhon Gerald');
-         
+
         echo $this->view('includes/header');
         echo $this->view('includes/nav');
-        echo $this->view('home', ['rows'=> $data]);
+        echo $this->view('home', ['rows' => $data]);
         echo $this->view('includes/footer');
     }
 }
